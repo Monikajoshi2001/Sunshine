b2fa2f462c845510b8043e73ddc89847c12b2c8a
Monikajoshi2001 <monikajoshimj1@gmail.com>
2021-03-30 17:25:51 +0530

    Made Layout files

---
+++ app/src/main/java/com/example/sunshine/city_finder.java
@@ -0,0 +1,14 @@
+package com.example.sunshine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class city_finder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_finder);
+    }
+}
