2ede8f63c4ddf7293ea18961e481bea21d3e93cf
Monikajoshi2001 <monikajoshimj1@gmail.com>
2021-04-04 12:48:19 +0530

    made weather_data.java class to get the data from JSONObject

--- app/src/main/java/com/example/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sunshine/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -28,16 +29,17 @@ import cz.msebera.android.httpclient.Header;
 public class MainActivity extends AppCompatActivity {
 
     final String API_Id = BuildConfig.API_KEY;
-    final String Weather_URL = "https://home.openweathermap.org/data/2.5/weather";
+    final String Weather_URL = "https://api.openweathermap.org/data/2.5/weather";
     final long min_time = 5000;   //5 sec
     final float min_distance = 1000;  //1 km
     final int request_code = 101;
 
     String Location_Provider = LocationManager.GPS_PROVIDER;
     LocationManager locationManager;
+    LocationListener locationListener;
 
 
-    TextView Temprature, weather, city;
+    TextView Temprature1, Temprature2, weather, city, humidity;
     ImageView weatherIcon;
     Button City_finder;
 
@@ -45,9 +47,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Temprature = findViewById(R.id.temprature);
+        Temprature1 = findViewById(R.id.temprature);
+        Temprature2 = findViewById(R.id.feels_temprature);
         weather = findViewById(R.id.weather_condition);
         city = findViewById(R.id.city_name);
+        humidity = findViewById(R.id.humidity2);
         weatherIcon = findViewById(R.id.weather_icon);
         City_finder = findViewById(R.id.find);
 
@@ -67,36 +71,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private void getWeatherForCurrentLocation() {
-        LocationListener locationListener = new LocationListener() {
+        locationManager = (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+        locationListener = new LocationListener() {
             //when user move from one location to another
             //we will fetch the weather of that location using latitude and longitude
             @Override
             public void onLocationChanged(@NonNull Location location) {
+
                 String Latitude = String.valueOf(location.getLatitude());
                 String Longitude = String.valueOf(location.getLongitude());
+                if(Latitude!=null&&Longitude!=null)
+                {
+                    Toast.makeText(MainActivity.this,"Latitude: "+ Latitude + "and Longitude: "+ Longitude,Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"could not get latitude and longitude",Toast.LENGTH_SHORT).show();
+                }
 
-                //fetching the weather data from website using API
+                //parameters of URL
                 RequestParams params = new RequestParams();
                 params.put("lat",Latitude);
-                params.put("long",Longitude);
-                params.put("APIid",API_Id);
-                letsDoSomeNetworking(params);
+                params.put("lon",Longitude);
+                params.put("appid",API_Id);
+                letsDoSomeNetworking(params);}
 
-            }
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
 
             }
-
             //when user allow the access to the location
             @Override
             public void onProviderEnabled(String provider) {
 
             }
-
             //when user allow the access to the location
             //but we are not able to fetch the location
             @Override
@@ -104,9 +114,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Not able to fetch the location", Toast.LENGTH_LONG).show();
             }
         };
-
-
-
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             // TODO: Consider calling
@@ -116,9 +123,10 @@ public class MainActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},request_code);
             return;
         }
-        locationManager.requestLocationUpdates(Location_Provider, min_time, min_distance, locationListener);
+            locationManager.requestLocationUpdates(Location_Provider, min_time, min_distance, locationListener);
     }
 
     //checking if user allows the location or not
@@ -149,18 +157,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 Toast.makeText(MainActivity.this,"Data recieved successfully",Toast.LENGTH_SHORT).show();
+                weather_data weatherD =weather_data.fromJson(response);
+                updateUI(weatherD);
                // super.onSuccess(statusCode, headers, response);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Toast.makeText(MainActivity.this,"Data not recieved successfully",Toast.LENGTH_SHORT).show();
                // super.onFailure(statusCode, headers, throwable, errorResponse);
             }
         });
 
     }
 
+    private void updateUI(weather_data weatherData)
+    {
+        Temprature1.setText(weatherData.getTemprature1());
+        Temprature2.setText(weatherData.getTemprature2());
+        weather.setText(weatherData.getWeather_type());
+        int resourceId=getResources().getIdentifier(weatherData.getIcon(),"drawable",getPackageName());
+        weatherIcon.setImageResource(resourceId);
+        city.setText(weatherData.getCity());
+        humidity.setText(weatherData.getHumidity());
 
+    }
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(locationManager!=null)
+        {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
 }
--- app/src/main/java/com/example/sunshine/weather_data.java
+++ app/src/main/java/com/example/sunshine/weather_data.java
@@ -1,6 +1,105 @@
 package com.example.sunshine;
 
-public class weather_data {
-    private String temprature,icon;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class weather_data {
+    private String temprature1;
+    private String temprature2;
+    private String icon;
+    private String city;
+    private String weather_type;
+    private String humidity;
+    private int conditions;   //for weather id
+
+    public static weather_data fromJson(JSONObject jsonObject)
+    {
+        try
+        {
+            weather_data weatherD = new weather_data();
+            weatherD.city= jsonObject.getString("name");
+            //we are getting the weather id here
+            weatherD.conditions=jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
+            //with the help of weather id we will find icon and weather type
+            weatherD.weather_type=jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
+            //weathericon will be updated using the conditions(weather id)
+            weatherD.icon= updateWeatherIcon(weatherD.conditions);
+            //getting temprature as a double and rounding off the value so obtained
+            //then storing the value as string
+            double temp1=jsonObject.getJSONObject("main").getDouble("temp")-273.15;
+            int roundedValue = (int)Math.rint(temp1);
+            weatherD.temprature1=Integer.toString(roundedValue);
+            double temp2= jsonObject.getJSONObject("main").getDouble("feels_like")-273.15;
+            int roundedvalue2 = (int)Math.rint(temp2);
+            weatherD.temprature2=Integer.toString(roundedvalue2);
+            weatherD.humidity=jsonObject.getJSONObject("main").getString("humidity");
+
+            return weatherD;
+
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    private static String updateWeatherIcon(int conditions)
+    {
+        if(conditions>=0&&conditions<300)
+        {
+            return "thunderstorm";
+        }
+        else if(conditions>=300&&conditions<500)
+        {
+            return "light_rain";
+        }
+        else if(conditions>=500&&conditions<600)
+        {
+            return "heavy_rain";
+        }
+        else if(conditions>=600&&conditions<700)
+        {
+            return "snowy";
+        }
+        else if(conditions>=700&&conditions<800)
+        {
+            return "foggy";
+        }
+        else if(conditions>=801&&conditions<804)
+        {
+            return "cloudy";
+        }
+        else if(conditions==800) {
+            return "sunny";
+        }
+        return "finding";
+    }
+
+    //all these are declared as private
+    //to use them in mainactivity.java am using getter
+    public String getTemprature1() {
+        return temprature1+"°C";
+    }
+
+    public String getTemprature2() {
+        return temprature2+"°C";
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getWeather_type() {
+        return weather_type;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
 }
